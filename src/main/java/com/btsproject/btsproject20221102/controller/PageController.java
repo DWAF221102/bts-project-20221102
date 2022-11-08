5ea43b60bfabccb7c1aad3bf15bedebb9ace4daf
lee4339 <skyho4339@naver.com>
2022-11-08 16:47:32 +0900

    main test 브랜치로 통합 후 파일 정리 및 통합

--- src/main/java/com/btsproject/btsproject20221102/controller/PageController.java
+++ src/main/java/com/btsproject/btsproject20221102/controller/PageController.java
@@ -13,74 +13,90 @@ public class PageController {
         return "index";
     }
 
+    // 통합 검색페이지
+    @GetMapping("totalsearch")
+    public String totalSearch() {
+        return "totalSearch";
+    }
+
 
     // 회원관련 페이지
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "account/login";
     }
 
     @GetMapping("/signup")
     public String signup() {
-        return "signup";
+        return "account/signup";
     }
 
     @GetMapping("/forgot")
     public String forgot() {
-        return "forgot";
+        return "account/findAccount";
+    }
+
+    @GetMapping("/forgot/password-change")
+    public String passwordChange() {
+        return "changePassword";
     }
 
     @GetMapping("/myprofile")
     public String myprofile() {
-        return "myprofile";
-    }
-
-    @GetMapping("/myprofile/settings")
-    public String settings() {
-        return "settings";
+        return "account/myProfile";
     }
 
     @GetMapping("/myprofile/password-change")
-    public String passwordChange() {
-        return "password-change";
+    public String settings() {
+        return "account/profileChangePassword";
     }
 
     @GetMapping("/myactivity")
     public String myactivity() {
-        return "myactivity";
+        return "account/detailsOfActivity";
     }
 
 
     // QNA
     @GetMapping("/question")
     public String question() {
-        return "question";
+        return "board/board";
     }
 
     @GetMapping("/question/write")
     public String questionWrite() {
-        return "questionWrite";
+        return "qna/qnaCreate";
     }
+
 
     // 나머지 카테고리(지식, 커뮤니티, 공지)
     @GetMapping("/knowledge")
     public String knowledge() {
-        return "knowledge";
+        return "board/board";
     }
 
     @GetMapping("/community")
     public String community() {
-        return "community";
+        return "board/board";
     }
 
     @GetMapping("/community/write")
     public String communitWrite() {
-        return "communitWrite";
+        return "board/write";
     }
 
     @GetMapping("/notice")
     public String notice() {
-        return "notice";
+        return "board/board";
+    }
+
+    // article 은 나중에 pathvariable을 통해 들어가도록 만들어야함
+    @GetMapping("/article")
+    public String article() { return "board/article"; }
+
+    @GetMapping("/qnaarticle")
+    public String qnaArticle() {
+        return "qna/qnaBoard";
     }
 
 
@@ -105,6 +121,8 @@ public class PageController {
         return "admin/adminBoardList";
     }
 
+
+    //////////////////////////////////////////////////////////////////
     @GetMapping("/admin/knowledge/write")
     public String knowledgeWrite() {
         return "admin/knowledgeWrite";
