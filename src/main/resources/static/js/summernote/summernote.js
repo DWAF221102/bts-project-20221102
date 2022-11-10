47090ede35407e7f6b258a0dabce2be203b5261e
wogy2829@naver.com <wogy2829@naver.com>
2022-11-10 15:56:26 +0900

    20221110 summernote api 추가, article 댓글 수정 jaehyo

---
+++ src/main/resources/static/js/summernote/summernote.js
@@ -0,0 +1,21 @@
+
+$('#summernote').summernote({
+    placeholder: '내용을 작성하세요.',
+    height: 400,
+    maxHeight: 400,
+
+    toolbar: [
+        ['style', ['bold', 'underline', 'clear']],
+        ['fontsize', ['fontsize']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['height', ['height']],
+        ['insert', ['picture']]
+    ],
+    popover: {
+        image: [
+          ['image', ['resizeFull', 'resizeHalf', 'resizeQuarter', 'resizeNone']],
+          ['float', ['floatLeft', 'floatRight', 'floatNone']],
+          ['remove', ['removeMedia']]
+        ]
+      }
+});
