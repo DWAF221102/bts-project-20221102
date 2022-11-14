86c7f887a98eb4b6499e09db4d52951e9c795220
lee4339 <skyho4339@naver.com>
2022-11-14 17:29:59 +0900

    221114 전체파일 수정(전체)

--- src/main/java/com/btsproject/btsproject20221102/repository/account/AccountRepository.java
+++ src/main/java/com/btsproject/btsproject20221102/repository/account/AccountRepository.java
@@ -1,8 +1,9 @@
 package com.btsproject.btsproject20221102.repository.account;
 
+import com.btsproject.btsproject20221102.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class AccountRepository {
-
+public interface AccountRepository {
+    public int save(User user);
 }
