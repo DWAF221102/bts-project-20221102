703c194c6f4ebaa4b25169fcc1416dac2140e663
lee4339 <skyho4339@naver.com>
2022-11-16 17:01:03 +0900

    20221116 validation error 표시(이건호)

--- src/main/java/com/btsproject/btsproject20221102/dto/account/SignupReqDto.java
+++ src/main/java/com/btsproject/btsproject20221102/dto/account/SignupReqDto.java
@@ -18,17 +18,17 @@ public class SignupReqDto {
     private String username;
 
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]{8,16}$",
-            message = "비밀번호는 숫자, 영문, 특수기호를 하나 이상 포함하여 8자 이상 16자 이하로 작성해야합니다.")
+            message = "숫자, 영문, 특수기호를 하나 이상 포함하여 8자 이상 16자 이하로 작성해야합니다.")
     private String password;
 
     @Pattern(regexp = "^[가-힇]{2,5}$",
-            message = "이름은 한글로 2자 이상 5자 이하로 입력가능합니다")
+            message = "한글로 2자 이상 5자 이하로 입력가능합니다")
     private String name;
 
     @Pattern(regexp="^010-?([0-9]{4})-?([0-9]{4})$", message = "휴대폰번호를 제대로 입력해주세요.")
     private String phone;
 
-    @Pattern(regexp = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$", message = "2자 이상 16자 이하, 영어 또는 숫자 또는 한글로 구성")
+    @Pattern(regexp = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$", message = "2자 이상 16자 이하, 영어 또는 숫자 또는 한글로 구성해야합니다.")
     private String nickName;
 
     public User toUserEntity() {
