c2fe402c3056bb9a09410878d02124b6000c3b5d
lee4339 <skyho4339@naver.com>
2022-11-22 16:34:53 +0900

    전체 통합1

--- src/main/java/com/btsproject/btsproject20221102/controller/MailController.java
+++ src/main/java/com/btsproject/btsproject20221102/controller/MailController.java
@@ -3,7 +3,6 @@ package com.btsproject.btsproject20221102.controller;
 import com.btsproject.btsproject20221102.dto.account.CertifiedDto;
 import com.btsproject.btsproject20221102.service.account.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/authentication")
 @RequiredArgsConstructor
-public class AuthController {
+public class MailController {
 
     private final AccountService accountService;
 
