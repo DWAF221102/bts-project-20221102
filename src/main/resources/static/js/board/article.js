5a2c635d621f7f99a04095516bfa7fd5eb5668a7
HaDH <gusdk123123@naver.com>
2022-12-12 14:32:11 +0900

    Revert "12/12 하덕현"
    This reverts commit 84f09f20c22f7b487c85e9de8a2d4a3f94b055d4.

--- src/main/resources/static/js/board/article.js
+++ src/main/resources/static/js/board/article.js
@@ -2,7 +2,7 @@ class ArticleApi {
     static #instance = null;
 
     static getInstance() {
-        if (this.#instance == null) {
+        if(this.#instance == null) {
             this.#instance = new ArticleApi();
         }
         return this.#instance;
@@ -15,9 +15,9 @@ class ArticleApi {
         $.ajax({
             async: false,
             type: "put",
-            url: "/api/viewcount",
+            url:"/api/viewcount",
             data: {
-                "id": this.id
+                "id" : this.id
             },
             dataType: "json",
             success: (response) => {
@@ -30,9 +30,9 @@ class ArticleApi {
     }
 
     loadArticleReq() {
-
-        let responseData = null;
-
+        
+        let responseData= null;
+        
         $.ajax({
             async: false,
             type: "get",
@@ -46,14 +46,14 @@ class ArticleApi {
                 console.log(error);
             }
         })
-
+        
         return responseData;
     }
 
     likeAddReq() {
         let data = {
-            "id": this.id,
-            "userId": principalUser.id
+            "id" : this.id,
+            "userId": principalUser.id 
         }
         $.ajax({
             async: false,
@@ -90,7 +90,7 @@ class CommentApi {
     static #instance = null;
 
     static getInstance() {
-        if (this.#instance == null) {
+        if(this.#instance == null) {
             this.#instance = new CommentApi();
         }
         return this.#instance;
@@ -98,9 +98,9 @@ class CommentApi {
 
     commentWriteReq(id, userId, textValue) {
         let data = {
-            "id": id,
-            "userId": userId,
-            "textValue": textValue
+            "id" : id,
+            "userId" : userId,
+            "textValue" : textValue
         }
 
         $.ajax({
@@ -121,9 +121,9 @@ class CommentApi {
 
     recommentWriteReq(commentId, userId, textValue) {
         let data = {
-            "commentId": commentId,
-            "userId": userId,
-            "textValue": textValue
+            "commentId" : commentId,
+            "userId" : userId,
+            "textValue" : textValue
         }
 
         $.ajax({
@@ -146,7 +146,7 @@ class ArticleService {
     static #instance = null;
 
     static getInstance() {
-        if (this.#instance == null) {
+        if(this.#instance == null) {
             this.#instance = new ArticleService();
         }
         return this.#instance;
@@ -154,20 +154,20 @@ class ArticleService {
 
     setCategory(responseData) {
         const articleCategory = document.querySelector(".article-category");
-
-        if (responseData.menuName == "지식") {
+        
+        if(responseData.menuName == "지식"){
             articleCategory.innerHTML = `
                 <a href="/knowledge">${responseData.menuName}</a>
                 <span>/</span>
                 <span>${responseData.categoryName}</span>
             `;
-        } else if (responseData.menuName == "커뮤니티") {
+        }else if(responseData.menuName == "커뮤니티"){
             articleCategory.innerHTML = `
                 <a href="/community">${responseData.menuName}</a>
                 <span>/</span>
                 <span>${responseData.categoryName}</span>
             `;
-        } else if (responseData.menuName == "공지사항") {
+        }else if(responseData.menuName == "공지사항"){
             articleCategory.innerHTML = `
                 <a href="/notice">${responseData.menuName}</a>
             `;
@@ -198,7 +198,7 @@ class ArticleService {
 
     setLike(responseData) {
         const userStar = document.querySelector(".user-star");
-
+        
         let likes = responseData.like;
         let likeCount = likes.length;
 
@@ -210,12 +210,12 @@ class ArticleService {
         const userStarButton = document.querySelector(".user-star-button");
         let userId = 0;
 
-        if (principalUser != null) {
+        if(principalUser != null) {
             userId = principalUser.id;
         }
-        if (likeCount != 0) {
+        if(likeCount != 0) {
             likes.forEach(like => {
-                if (like.from_id == userId) {
+                if(like.from_id == userId) {
                     userStarButton.classList.add("blue-button");
                     userStarButton.onclick = () => {
                         userStarButton.classList.remove("blue-button");
@@ -225,7 +225,7 @@ class ArticleService {
                 }
                 else {
                     userStarButton.onclick = () => {
-                        if (userId != 0) {
+                        if(userId != 0) {
                             userStarButton.classList.add("blue-button");
                             ArticleApi.getInstance().likeAddReq();
                             location.reload();
@@ -233,9 +233,9 @@ class ArticleService {
                     }
                 }
             });
-        } else {
+        }else{
             userStarButton.onclick = () => {
-                if (userId != 0) {
+                if(userId != 0) {
                     userStarButton.classList.add("blue-button");
                     ArticleApi.getInstance().likeAddReq();
                     location.reload();
@@ -252,11 +252,11 @@ class ArticleService {
             <h1>${responseData.title}</h1>
         `;
         articleContent.innerHTML = `<p>${responseData.content}</p>`;
-
+        
     }
 
     setBottumCategory(responseData) {
-        if (responseData.menuName != "공지사항") {
+        if(responseData.menuName != "공지사항"){
             const articleInfo = document.querySelector(".article-info");
             articleInfo.innerHTML = `
                 <div class="article-categories">
@@ -269,12 +269,12 @@ class ArticleService {
 
     setUpdateButton(responseData) {
         let userId = 0;
-        if (principalUser != null) {
+        if(principalUser != null) {
             userId = principalUser.id;
         }
-        if (userId != 0) {
-            if (userId == responseData.userId) {
-                const articleInfo = document.querySelector(".article-info");
+        if(userId != 0) {
+            if(userId == responseData.userId){
+                const articleInfo = document.querySelector(".article-info"); 
 
                 articleInfo.innerHTML += `
                     <div>
@@ -287,12 +287,12 @@ class ArticleService {
                 const updateButton = document.querySelector(".update-button");
 
                 updateButton.onclick = () => {
-                    if (confirm("게시물을 수정하시겠습니까?")) {
-                        if (responseData.menuName == "지식") {
+                    if(confirm("게시물을 수정하시겠습니까?")) {
+                        if(responseData.menuName == "지식"){
                             location.href = "/knowledge/update/" + responseData.id;
-                        } else if (responseData.menuName == "커뮤니티") {
+                        }else if(responseData.menuName == "커뮤니티"){
                             location.href = "/community/update/" + responseData.id;
-                        } else if (responseData.menuName == "지식") {
+                        }else if(responseData.menuName == "지식"){
                             location.href = "/notice/update/" + responseData.id;
                         }
                     }
@@ -308,7 +308,7 @@ class CommentService {
     static #instance = null;
 
     static getInstance() {
-        if (this.#instance == null) {
+        if(this.#instance == null) {
             this.#instance = new CommentService();
         }
         return this.#instance;
@@ -323,19 +323,19 @@ class CommentService {
 
     writeComment(responseData) {
         const commentWrite = document.querySelector(".comment-write");
-
+        
         let id = responseData.id;
         let userId = 0;
         let userImg = null;
 
-        if (principalUser != null) {
+        if(principalUser != null) {
             userId = principalUser.id;
             userImg = principalUser.user_img;
-        } else {
+        }else {
             commentWrite.classList.remove("grey-border");
         }
 
-        if (userId != 0) {
+        if(userId != 0) {
             commentWrite.innerHTML = `
                 <div class="user-img">
                     <img src="/image/user/${userImg}">
@@ -352,20 +352,20 @@ class CommentService {
             `;
 
             const writeButtons = document.querySelectorAll(".write-button button");
-
+           
 
             writeButtons[0].onclick = () => {
-                if (confirm("취소하시겠습니까?")) {
+                if(confirm("취소하시겠습니까?")) {
                     location.reload();
                 }
             }
             writeButtons[1].onclick = () => {
                 const textarea = document.querySelector(".comment-textarea");
                 let textValue = textarea.value;
-                if (confirm("댓글을 작성하시겠습니까???")) {
-                    if (textValue == "" || textValue == " " || textValue == null || textValue.replaceAll(" ", "") == "") {
+                if(confirm("댓글을 작성하시겠습니까???")) {
+                    if(textValue == "" || textValue == " " || textValue == null || textValue.replaceAll(" ", "") == "") {
                         alert("내용을 입력해주세요.");
-                    } else {
+                    }else {
                         CommentApi.getInstance().commentWriteReq(id, userId, textarea.value);
                         location.reload();
                     }
@@ -378,9 +378,9 @@ class CommentService {
 
     setComment(responseData) {
         let comment = responseData.comment;
-        if (comment.length != 0) {
+        if(comment.length != 0) {
             const commentUl = document.querySelector(".comment-ul");
-            for (let i = comment.length; i > 0; i--) {
+            for(let i = comment.length; i >  0; i--) {
                 let index = i - 1;
                 commentUl.innerHTML += `
                     <li>
@@ -418,12 +418,12 @@ class CommentService {
                     </li> 
                 `;
 
-
+                
 
                 const recommentUl = document.querySelector(".recomment-ul");
-
+                
                 let recomment = comment[index].recomment;
-                if (recomment.length != 0) {
+                if(recomment.length != 0) {
                     this.recommentButton(index, recomment);
 
                     recomment.forEach(data => {
@@ -451,7 +451,7 @@ class CommentService {
                         `;
                     });
                 }
-
+               
             }
         }
 
@@ -472,25 +472,25 @@ class CommentService {
 
         this.showRecomment(showRecommentButton[index], showRecommentSpan[index], recommentCount, recommentUl[index]);
 
-
+        
     }
 
     showRecomment(button, div, recommentCount, recommentUl) {
         button.onclick = () => {
             div.innerText = "댓글 모두 숨기기";
-
+            
             recommentUl.classList.remove("none");
 
             this.removeRecomment(button, div, recommentCount, recommentUl);
         }
     }
-
+        
     removeRecomment(button, div, recommentCount, recommentUl) {
         button.onclick = () => {
             div.innerText = `댓글 ${recommentCount}개 보기`;
-
-            recommentUl.classList.add("none");
-
+            
+            recommentUl.classList.add("none");     
+            
             this.showRecomment(button, div, recommentCount, recommentUl);
         }
     }
@@ -502,14 +502,14 @@ class CommentService {
         let userId = 0;
         let userImg = null;
 
-        if (principalUser != null) {
+        if(principalUser != null) {
             userId = principalUser.id;
             userImg = principalUser.user_img;
         }
 
-        for (let i = 0; i < writeRecommentButton.length; i++) {
+        for(let i = 0; i < writeRecommentButton.length; i++) {
             writeRecommentButton[i].onclick = () => {
-                if (userId != 0) {
+                if(userId != 0) {
                     deleteRecomment[i].innerHTML = `
                         <div class="recomment-write ">
                             <div class="user-img">
@@ -524,27 +524,27 @@ class CommentService {
                                 </div>
                             </div>    
                         </div>
-                    `;
+                    `;  
                     this.recommentButtonEvent(responseData);
                     this.deleteRecommentWrite(responseData);
-                } else {
+                }else {
                     alert("로그인후 작성가능합니다.");
                 }
-
+                
             }
         }
-
+        
     }
-
+    
 
     deleteRecommentWrite(responseData) {
         const writeRecommentButton = document.querySelectorAll(".write-reccoment-button");
         const deleteRecomment = document.querySelectorAll(".delete-recomment");
 
-        for (let i = 0; i < writeRecommentButton.length; i++) {
+        for(let i = 0; i < writeRecommentButton.length; i++) {
             writeRecommentButton[i].onclick = () => {
                 deleteRecomment[i].innerHTML = "";
-
+                
                 this.showRecommentWrite(responseData);
             }
         }
@@ -552,37 +552,36 @@ class CommentService {
 
     recommentButtonEvent(responseData) {
         let userId = 0;
-        if (principalUser != null) {
+        if(principalUser != null) {
             userId = principalUser.id;
         }
         const recommentButton = document.querySelectorAll(".button-recomment");
+        
 
-
-        for (let i = 0; i < recommentButton.length; i++) {
+        for(let i = 0; i < recommentButton.length; i++) {
             recommentButton[i].onclick = () => {
                 let recomentTextarea = document.querySelectorAll(".recomment-textarea");
                 let textValue = recomentTextarea[i].value;
-                let index = recommentButton.length - i;
-                let commentId = responseData.comment[index].comment_id;
-                if (textValue != "" && textValue != " " && textValue != null && textValue.replaceAll(" ", "") != "") {
-                    if (confirm("댓글을 작성하시겠습니까?")) {
+                let commentId = responseData.comment[i].comment_id;
+                if(textValue != "" && textValue != " " && textValue != null && textValue.replaceAll(" ", "") != "") {
+                    if(confirm("댓글을 작성하시겠습니까?")){
                         CommentApi.getInstance().recommentWriteReq(commentId, userId, textValue);
                         location.reload();
                     }
-                } else {
+                }else {
                     alert("댓글을 입력해주세요.");
                 }
             }
         }
     }
-
+    
 }
 
 class TotalService {
     static #instance = null;
 
     static getInstance() {
-        if (this.#instance == null) {
+        if(this.#instance == null) {
             this.#instance = new TotalService();
         }
         return this.#instance;
@@ -591,7 +590,7 @@ class TotalService {
     addService() {
         ArticleApi.getInstance().viewCountReq();
         let responseData = ArticleApi.getInstance().loadArticleReq();
-        if (responseData != null) {
+        if(responseData != null) {
             ArticleService.getInstance().setCategory(responseData);
             ArticleService.getInstance().setUser(responseData);
             ArticleService.getInstance().setLike(responseData);
@@ -603,7 +602,7 @@ class TotalService {
             CommentService.getInstance().showRecommentWrite(responseData);
             CommentService.getInstance().recommentButtonEvent(responseData);
         }
-
+        
     }
 }
 
@@ -611,7 +610,7 @@ class TimeService {
     static #instance = null;
 
     static getInstance() {
-        if (this.#instance == null) {
+        if(this.#instance == null) {
             this.#instance = new TimeService();
         }
         return this.#instance;
@@ -620,7 +619,7 @@ class TimeService {
     setTime(createDate) {
         let date = new Date();
         let year = date.getFullYear();
-        let month = date.getMonth() + 1;
+        let month = date.getMonth() +1;
         let day = date.getDate();
         let hour = date.getHours();
         let minute = date.getMinutes();
@@ -629,44 +628,44 @@ class TimeService {
         let c2 = createDate.substring(createDate.indexOf("T") + 1, createDate.lastIndexOf(":"));
 
         let cYear = Number(c1.substring(0, c1.indexOf("-")));
-        let cMonth = Number(c1.substring(c1.indexOf("-") + 1, c1.lastIndexOf("-")));
+        let cMonth = Number(c1.substring(c1.indexOf("-") + 1 , c1.lastIndexOf("-")));
         let cDay = Number(c1.substring(c1.lastIndexOf("-") + 1));
         let cHour = Number(c2.substring(0, c2.indexOf(":")));
         let cMinute = Number(c2.substring(c2.indexOf(":") + 1));
+        
 
 
-
-        if (year != cYear) {
-            if (year - cYear == 1) {
-                if ((12 - cMonth + month) < 12) {
+        if(year != cYear) {
+            if(year - cYear == 1){
+                if((12 - cMonth + month) < 12){
                     return (12 - cMonth + month) + "개월 전";
-                } else {
+                }else {
                     return "1년 전";
                 }
-            } else {
+            }else {
                 return (year - cYear) + "년 전";
             }
-        } else {
-            if (month != cMonth) {
-                if (month - cMonth == 1) {
-                    if ((31 - cDay + day) < 31) {
+        }else {
+            if(month != cMonth) {
+                if(month - cMonth == 1){
+                    if((31 - cDay + day) < 31){
                         return (31 - cDay + day) + "일 전";
-                    } else {
+                    }else {
                         return (month - cMonth) + "개월 전";
                     }
-                } else {
+                }else {
                     return (month - cMonth) + "개월 전";
                 }
-            } else {
-                if (day != cDay) {
+            }else{
+                if(day != cDay) {
                     return (day - cDay) + "일 전";
-                } else {
-                    if (hour != cHour) {
+                }else {
+                    if(hour != cHour) {
                         return (hour - cHour) + "시간 전";
-                    } else {
-                        if (minute != cMinute) {
+                    }else {
+                        if(minute != cMinute){
                             return (minute - cMinute) + "분 전";
-                        } else {
+                        }else {
                             return "1분 미만 전";
                         }
                     }
